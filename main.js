d4be426a4bb92bcbc2fe849043c3a0c340cb40f4
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2016-10-11 18:27:34 +0900

    Use just uglifyjs instead of webpack

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const selected = document.getSelection();
-const word = window.prompt('Search in Longman: ', selected);
+(function() {
+  const selected = document.getSelection();
+  const word = window.prompt('Search in Longman: ', selected);
 
-if (word) {
-  window.open('http://www.ldoceonline.com/search/?q=' + word);
-}
+  if (word) {
+    window.open('http://www.ldoceonline.com/search/?q=' + word);
+  }
+})();
